71e0ab99c63bf3562541c5730be16e48875aaf1a
이준영 <74142881+User-Lawn@users.noreply.github.com>
2022-05-21 00:38:28 +0900

    interface builder basics

--- IBBasics/IBBasics/ViewController.swift
+++ IBBasics/IBBasics/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //IBOutlet
+    @IBOutlet weak var myButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myButton.setTitleColor(.red, for: .normal)
     }
-
-
+    //터치업 인사이드로 연결되있어서 버튼이 떼질때 액션이 됨
+    //sender는 저 함수를 실행시킨 오브젝트가 샌더에 넘어온다 매개변수로
+    @IBAction func buttonPressed(_ sender: Any) {
+        print("the button was pressed")
+    }
+    
 }
 
